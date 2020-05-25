a29687088ed84ecf4ed4e38560e8dbf2f6de128e
bennyjien <bennyjien@users.noreply.github.com>
2020-05-25 17:48:14 +0700

    rename style from App to app

--- template/src/App.js
+++ template/src/App.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import HomePage from './pages/home.page';
 import logo from './assets/images/logo-react.svg';
-import './styles/App.scss';
+import './styles/app.scss';
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-				<div className="App-header-inner">
+    <div className="app">
+      <header className="app-header">
+				<div className="app-header-inner">
 					<img src={logo} className="logo" alt="React Logo" />
 				</div>
       </header>
-			<main className="App-main">
-				<div className="App-main-inner">
+			<main className="app-main">
+				<div className="app-main-inner">
 					<HomePage />
 				</div>
 			</main>
-			<footer className="App-footer">
-				<div className="App-footer-inner">
+			<footer className="app-footer">
+				<div className="app-footer-inner">
 					<p><small>Copyright &copy; {new Date().getFullYear()}. Design and code by <a href="//icubic.co.id">ICUBIC</a></small>.</p>
 				</div>
 			</footer>
